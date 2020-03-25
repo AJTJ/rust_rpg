6d78cc78b75e78e7b75a823de59dd902eb838c87
AJTJ <janke.aaron@gmail.com>
2020-03-24 21:38:59 -0600

    feat: updated combat

--- initial_start/src/main.rs
+++ initial_start/src/main.rs
@@ -6,37 +6,51 @@ use std::io;
 fn main() {
     let mut health: i32 = 30;
     let max_health: i32 = 30;
-    let magic: i32 = 30;
+    let mut magic: i32 = 30;
     let mut stage: u32 = 1;
-    let in_combat: bool = false;
     loop {
         if health < 1 {
             println!("You are dead!");
             break;
         }
 
+        if stage >= 10 {
+            println!("You conquered the dungeon!!");
+            break;
+        }
+
         if health < max_health {
-            println!("Use healing magic? Type 'y' for yes");
-            let mut response = String::new();
-            io::stdin()
-                .read_line(&mut response)
-                .expect("Response Error");
-            if check_yes_no(&response) {
-                let current_heal: i32 = self_heal();
-                println!("You heal {}!", current_heal);
-                health = current_heal + health;
-                if health > max_health {
-                    health = max_health;
+            if magic >= 5 {
+                println!("Use healing magic? Type 'y' for yes");
+                let mut response = String::new();
+                io::stdin()
+                    .read_line(&mut response)
+                    .expect("Response Error");
+                if check_yes_no(&response) {
+                    let current_heal: i32 = self_heal();
+                    println!("You heal {}!", current_heal);
+                    health = current_heal + health;
+                    if health > max_health {
+                        health = max_health;
+                    }
+                    magic = magic - 5;
+                    stat_update(health, magic);
                 }
-                stat_update(health, magic);
+            } else {
+                println!("Not enough magic to heal!")
             }
         }
+        if stage == 1 {
+            println!("Welcome to the dungeon! There are 10 stages, will you survive?")
+        }
         confirmation();
-        // 50% chance to encounter a creature...
-        if get_percentage() > 50 {
-            enter_combat(stage, health, magic);
+        // 75% chance to encounter a goblin...
+        if get_percentage() > 25 {
+            let combat_outcome: (i32, i32) = enter_combat(stage, health, magic);
+            health = combat_outcome.0;
+            magic = combat_outcome.1;
         } else {
-            println!("Nothing encountered here...")
+            println!("Nothing encountered in stage {}", stage)
         }
         stat_update(health, magic);
         stage = stage + 1;
@@ -44,7 +58,7 @@ fn main() {
 }
 
 fn confirmation() {
-    println!("Press Enter to continue");
+    println!("--- Press Enter to continue ---");
     let mut any_input = String::new();
     io::stdin()
         .read_line(&mut any_input)
@@ -52,39 +66,51 @@ fn confirmation() {
 }
 
 fn enter_combat(stage: u32, health: i32, magic: i32) -> (i32, i32) {
-    loop {
-        let mut round: i32 = 1;
-        let hero_initiative;
-        let mut current_health: i32 = health;
-        let mut enemy_health: i32 = 10;
-        println!("You encounter a goblin!");
-        confirmation();
-        // get initiative
-        if get_percentage() > 50 {
-            println!("The goblin attacks first");
-            hero_initiative = false
-        } else {
-            println!("You attack first!");
-            hero_initiative = true
-        }
-
-        loop {
-            if hero_initiative == true {
-                enemy_health = hero_attack(enemy_health);
-                if enemy_health < 1 {
-                    println!("You defeated the goblin!")
-                    break;
-                }
-                current_health = creature_attack(current_health);
-                if current_health < 1 {
-                    break;
-                };
-                round = round + 1;
-            }
-        }
-        let final_health: i32 = current_health;
-        (final_health, magic)
+    let hero_initiative;
+    let mut current_health: i32 = health;
+    let current_magic: i32 = magic;
+    let mut enemy_health: i32 = 10;
+    let mut round: i32 = 1;
+    println!("At stage {}, you encounter a goblin!", stage);
+    if get_percentage() > 50 {
+        println!("...The goblin attacks first...");
+        hero_initiative = false
+    } else {
+        println!("...You attack first!...");
+        hero_initiative = true
     }
+    confirmation();
+    loop {
+        println!("*Round {}*", round);
+        if hero_initiative == true {
+            enemy_health = hero_attack(enemy_health);
+            if enemy_health < 1 {
+                println!("You defeated the goblin!");
+                stat_update(current_health, current_magic);
+                confirmation();
+                break;
+            }
+            current_health = creature_attack(current_health);
+            if current_health < 1 {
+                break;
+            };
+        } else {
+            current_health = creature_attack(current_health);
+            if current_health < 1 {
+                break;
+            };
+            enemy_health = hero_attack(enemy_health);
+            if enemy_health < 1 {
+                println!("You defeated the goblin!");
+                stat_update(current_health, current_magic);
+                confirmation();
+                break;
+            }
+        };
+        round = round + 1;
+    }
+    let final_health: i32 = current_health;
+    (final_health, magic)
 }
 
 fn get_percentage() -> i32 {
@@ -92,7 +118,6 @@ fn get_percentage() -> i32 {
 }
 
 fn check_yes_no(response: &str) -> bool {
-    println!("in check {}", response);
     match response.trim() {
         "y" | "yes" | "Y" | "Yes" => true,
         "n" | "no" | "N" | "No" => false,
@@ -110,7 +135,7 @@ fn sword_damage() -> i32 {
 
 fn hero_attack(health: i32) -> i32 {
     let damage = sword_damage();
-    println!("It attacks you and does {} damage", damage);
+    println!("You attack with your sword! You do {} damage", damage);
     let new_health: i32 = health - damage;
     new_health
 }
