5a7cf85057faa28b2072e113fba2e26adeff7b0f
AJTJ <janke.aaron@gmail.com>
2020-03-31 01:33:43 -0400

    feat: cleaning up

--- initial_start/src/main.rs
+++ initial_start/src/main.rs
@@ -1,47 +1,34 @@
+use math::round;
 use rand::Rng;
 use std::io;
 
-//NOTES
-/*
-21:49 < Mutabah> Quick check:
-21:49 < Mutabah> - I suggest using a `struct` to contain the game state variables
-21:49 < Mutabah> - abstract out the yes/no query
-21:50 < AJTJ> game state variables like health etc?
-21:50 < Mutabah> - use `+=` instead of `stage = stage + 1;`
-21:50 < Mutabah> (yes)
-21:50 < AJTJ> should I be using more mutable vars?
-21:52 < Mutabah> - You could remove duplication in `enter_combat` by keepting track of whose turn it is (initialise that where you currently set `hero_initiative`)
-21:53 < GreenJello> AJTJ, with a game state struct, you might take a mutable reference to that instead
-21:54 < Arnavion> Eh, for a game that's one function, using local vars for the state instead of a struct isn't a big deal
-21:54 < AJTJ> Arnavion: that's a good point, but it's good to think of scaling
-21:54 < AJTJ> cause it will
-21:57 < GreenJello> you could abstract a lot of this logic with structs, though, e.g. https://gist.github.com/brigand/2573131a7045d2617d80a4fab2b088db
-21:57 < Arnavion> I'd more prioritize fixing the fact that the two branches inside the loop in enter_combat() are mirror images of each other
-21:58 < AJTJ> Arnavion: yea, I wasn't sure how to resolve that. I'm super new to rust.
-21:58 < Arnavion> if hero_initialive { hero_attack(); } loop { creature_attack(); hero_attack(); }
-22:00 < Mutabah> or `let mut hero_turn = hero_initiative; loop { if hero_turn { ... } else { ... }; half_rounds += 1; hero_turn = !hero_turn; }`
-22:01 < Arnavion> Yeah, that would avoid the duplication of hero_attack() too
-22:01 < Mutabah> and allow common code after each half-round (e.g. if attacking a certain monster can lead to self-damage)
-*/
+struct GameState {
+    health: i32,
+    max_health: i32,
+    magic: i32,
+    stage: i32,
+}
 
 fn main() {
-    let mut health: i32 = 30;
-    let max_health: i32 = 30;
-    let mut magic: i32 = 30;
-    let mut stage: u32 = 1;
+    let mut gs = GameState {
+        health: 30,
+        max_health: 30,
+        magic: 30,
+        stage: 1,
+    };
     loop {
-        if health < 1 {
+        if gs.health < 1 {
             println!("You are dead!");
             break;
         }
 
-        if stage >= 10 {
+        if gs.stage >= 10 {
             println!("You conquered the dungeon!!");
             break;
         }
 
-        if health < max_health {
-            if magic >= 5 {
+        if gs.health < gs.max_health {
+            if gs.magic >= 5 {
                 println!("Use healing magic? Type 'y' for yes");
                 let mut response = String::new();
                 io::stdin()
@@ -50,31 +37,31 @@ fn main() {
                 if check_yes_no(&response) {
                     let current_heal: i32 = self_heal();
                     println!("You heal {}!", current_heal);
-                    health = current_heal + health;
-                    if health > max_health {
-                        health = max_health;
+                    gs.health = current_heal + gs.health;
+                    if gs.health > gs.max_health {
+                        gs.health = gs.max_health;
                     }
-                    magic = magic - 5;
-                    stat_update(health, magic);
+                    gs.magic = gs.magic - 5;
+                    stat_update(gs.health, gs.magic);
                 }
             } else {
                 println!("Not enough magic to heal!")
             }
         }
-        if stage == 1 {
-            println!("Welcome to the dungeon! There are 10 stages, will you survive?")
+        if gs.stage == 1 {
+            println!("Welcome to the dungeon! There are 10 levels, will you survive?")
         }
         confirmation();
-        // 75% chance to encounter a goblin...
-        if get_percentage() > 25 {
-            let combat_outcome: (i32, i32) = enter_combat(stage, health, magic);
-            health = combat_outcome.0;
-            magic = combat_outcome.1;
+        // 75% and rising chance to encounter a goblin...
+        if get_percentage() > 25 + 2 * gs.stage {
+            let combat_outcome: (i32, i32) = enter_combat(gs.stage, gs.health, gs.magic);
+            gs.health = combat_outcome.0;
+            gs.magic = combat_outcome.1;
         } else {
-            println!("Nothing encountered in stage {}", stage)
+            println!("Nothing encountered in stage {}", gs.stage)
         }
-        stat_update(health, magic);
-        stage = stage + 1;
+        stat_update(gs.health, gs.magic);
+        gs.stage += gs.stage;
     }
 }
 
@@ -86,51 +73,71 @@ fn confirmation() {
         .expect("Click Enter Error");
 }
 
-fn enter_combat(stage: u32, health: i32, magic: i32) -> (i32, i32) {
-    let hero_initiative;
-    let mut current_health: i32 = health;
-    let current_magic: i32 = magic;
-    let mut enemy_health: i32 = 10;
-    let mut round: i32 = 1;
+struct CombatState {
+    hero_turn: bool,
+    current_health: i32,
+    current_magic: i32,
+    enemy_health: i32,
+    round: i32,
+}
+
+struct Creature {
+    cre_health: i32,
+    cre_low_att_ran: i32,
+    cre_high_att_ran: i32,
+}
+
+fn enter_combat(stage: i32, health: i32, magic: i32) -> (i32, i32) {
+    // set range but round down
+    let half_round = stage / 2;
+    let cur_mon_att_bonus = round::floor(half_round, 0);
+    println!("this thing {}", cur_mon_att_bonus);
+    let mut creature = Creature {
+        cre_health: 5 + stage,
+        cre_low_att_ran: 2 + cur_mon_att_bonus,
+        cre_high_att_ran: 4 + cur_mon_att_bonus,
+    };
+
+    let mut cs = CombatState {
+        hero_turn: true,
+        current_health: health,
+        current_magic: magic,
+        enemy_health: 10,
+        round: 1,
+    };
     println!("At stage {}, you encounter a goblin!", stage);
     if get_percentage() > 50 {
         println!("...The goblin attacks first...");
-        hero_initiative = false
+        cs.hero_turn = false
     } else {
         println!("...You attack first!...");
-        hero_initiative = true
+        cs.hero_turn = true
     }
     confirmation();
     loop {
-        println!("*Round {}*", round);
-        if hero_initiative == true {
-            enemy_health = hero_attack(enemy_health);
-            if enemy_health < 1 {
+        println!("*Round {}*", cs.round);
+        if cs.hero_turn {
+            cs.enemy_health = hero_attack(creature.cre_health);
+            if cs.enemy_health < 1 {
                 println!("You defeated the goblin!");
-                stat_update(current_health, current_magic);
+                stat_update(cs.current_health, cs.current_magic);
                 confirmation();
                 break;
             }
-            current_health = creature_attack(current_health);
-            if current_health < 1 {
-                break;
-            };
         } else {
-            current_health = creature_attack(current_health);
-            if current_health < 1 {
+            cs.current_health = creature_attack(
+                cs.current_health,
+                creature.cre_low_att_ran,
+                creature.cre_high_att_ran,
+            );
+            if cs.current_health < 1 {
                 break;
             };
-            enemy_health = hero_attack(enemy_health);
-            if enemy_health < 1 {
-                println!("You defeated the goblin!");
-                stat_update(current_health, current_magic);
-                confirmation();
-                break;
-            }
         };
-        round = round + 1;
+        cs.hero_turn = !cs.hero_turn;
+        cs.round += cs.round;
     }
-    let final_health: i32 = current_health;
+    let final_health: i32 = cs.current_health;
     (final_health, magic)
 }
 
@@ -161,8 +168,8 @@ fn hero_attack(health: i32) -> i32 {
     new_health
 }
 
-fn creature_attack(health: i32) -> i32 {
-    let damage = rand::thread_rng().gen_range(2, 5);
+fn creature_attack(health: i32, cre_low_rng: i32, cre_high_rng: i32) -> i32 {
+    let damage = rand::thread_rng().gen_range(cre_low_rng, cre_high_rng);
     println!("It attacks you and does {} damage", damage);
     let new_health: i32 = health - damage;
     new_health
