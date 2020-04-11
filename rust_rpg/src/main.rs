ffc83c5e1cdfea0b10212c1a519fb9d3638b3fe0
AJTJ <janke.aaron@gmail.com>
2020-04-11 16:48:21 -0400

    feat: renamed and cleaned up

--- rust_rpg/src/main.rs
+++ rust_rpg/src/main.rs
@@ -1,4 +1,3 @@
-use math::round;
 use rand::Rng;
 use std::io;
 
@@ -6,6 +5,7 @@ struct GameState {
     health: i32,
     max_health: i32,
     magic: i32,
+    max_magic: i32,
     stage: i32,
 }
 
@@ -14,6 +14,7 @@ fn main() {
         health: 30,
         max_health: 30,
         magic: 30,
+        max_magic: 30,
         stage: 1,
     };
     loop {
@@ -22,6 +23,8 @@ fn main() {
             break;
         }
 
+        println!("stage: {}", gs.stage);
+
         if gs.stage >= 10 {
             println!("You conquered the dungeon!!");
             break;
@@ -29,7 +32,7 @@ fn main() {
 
         if gs.health < gs.max_health {
             if gs.magic >= 5 {
-                println!("Use healing magic? Type 'y' for yes");
+                println!("Use healing magic? Type 'y' for yes (heals 1-4 for 5 magic)");
                 let mut response = String::new();
                 io::stdin()
                     .read_line(&mut response)
@@ -42,7 +45,7 @@ fn main() {
                         gs.health = gs.max_health;
                     }
                     gs.magic = gs.magic - 5;
-                    stat_update(gs.health, gs.magic);
+                    stat_update(gs.health, gs.max_health, gs.magic, gs.max_magic);
                 }
             } else {
                 println!("Not enough magic to heal!")
@@ -52,7 +55,7 @@ fn main() {
             println!("Welcome to the dungeon! There are 10 levels, will you survive?")
         }
         confirmation();
-        // 75% and rising chance to encounter a goblin...
+        // 75% and rising chance to encounter an enemy...
         if get_percentage() > 25 + 2 * gs.stage {
             let combat_outcome: (i32, i32) = enter_combat(gs.stage, gs.health, gs.magic);
             gs.health = combat_outcome.0;
@@ -60,8 +63,8 @@ fn main() {
         } else {
             println!("Nothing encountered in stage {}", gs.stage)
         }
-        stat_update(gs.health, gs.magic);
-        gs.stage += gs.stage;
+        stat_update(gs.health, gs.max_health, gs.magic, gs.max_magic);
+        gs.stage += 1;
     }
 }
 
@@ -76,38 +79,51 @@ fn confirmation() {
 struct CombatState {
     hero_turn: bool,
     current_health: i32,
-    current_magic: i32,
+    // current_magic: i32,
     enemy_health: i32,
     round: i32,
 }
 
-struct Creature {
+struct Creature<'a> {
     cre_health: i32,
     cre_low_att_ran: i32,
     cre_high_att_ran: i32,
+    cre_name: &'a str,
 }
 
 fn enter_combat(stage: i32, health: i32, magic: i32) -> (i32, i32) {
-    // set range but round down
-    let half_round = stage / 2;
-    let cur_mon_att_bonus = round::floor(half_round, 0);
-    println!("this thing {}", cur_mon_att_bonus);
-    let mut creature = Creature {
+    let quarter_round = stage / 4;
+    let third_round = stage / 3;
+    let set_creature_name;
+    if stage > 7 {
+        set_creature_name = "Master Goblin";
+    } else if stage > 4 {
+        set_creature_name = "Big Goblin";
+    } else {
+        set_creature_name = "Goblin"
+    };
+
+    let creature = Creature {
         cre_health: 5 + stage,
-        cre_low_att_ran: 2 + cur_mon_att_bonus,
-        cre_high_att_ran: 4 + cur_mon_att_bonus,
+        cre_low_att_ran: 2 + quarter_round,
+        cre_high_att_ran: 4 + third_round,
+        cre_name: set_creature_name,
     };
 
     let mut cs = CombatState {
         hero_turn: true,
         current_health: health,
-        current_magic: magic,
-        enemy_health: 10,
+        // current_magic: magic,
+        enemy_health: creature.cre_health,
         round: 1,
     };
-    println!("At stage {}, you encounter a goblin!", stage);
+    println!("At stage {}, you encounter a {}!", stage, creature.cre_name);
+    println!(
+        "It has {} hit points and does {} to {} damage!",
+        creature.cre_health, creature.cre_low_att_ran, creature.cre_high_att_ran
+    );
     if get_percentage() > 50 {
-        println!("...The goblin attacks first...");
+        println!("...The {} attacks first...", creature.cre_name);
         cs.hero_turn = false
     } else {
         println!("...You attack first!...");
@@ -117,11 +133,11 @@ fn enter_combat(stage: i32, health: i32, magic: i32) -> (i32, i32) {
     loop {
         println!("*Round {}*", cs.round);
         if cs.hero_turn {
-            cs.enemy_health = hero_attack(creature.cre_health);
+            cs.enemy_health = hero_attack(cs.enemy_health);
             if cs.enemy_health < 1 {
-                println!("You defeated the goblin!");
-                stat_update(cs.current_health, cs.current_magic);
-                confirmation();
+                println!("You defeated the {}!", creature.cre_name);
+                // stat_update(cs.current_health, cs.current_magic);
+                // confirmation();
                 break;
             }
         } else {
@@ -135,7 +151,7 @@ fn enter_combat(stage: i32, health: i32, magic: i32) -> (i32, i32) {
             };
         };
         cs.hero_turn = !cs.hero_turn;
-        cs.round += cs.round;
+        cs.round += 1;
     }
     let final_health: i32 = cs.current_health;
     (final_health, magic)
@@ -163,18 +179,21 @@ fn sword_damage() -> i32 {
 
 fn hero_attack(health: i32) -> i32 {
     let damage = sword_damage();
-    println!("You attack with your sword! You do {} damage", damage);
+    println!("- You attack with your sword! You do {} damage", damage);
     let new_health: i32 = health - damage;
     new_health
 }
 
 fn creature_attack(health: i32, cre_low_rng: i32, cre_high_rng: i32) -> i32 {
     let damage = rand::thread_rng().gen_range(cre_low_rng, cre_high_rng);
-    println!("It attacks you and does {} damage", damage);
+    println!("- It attacks you and does {} damage", damage);
     let new_health: i32 = health - damage;
     new_health
 }
 
-fn stat_update(health: i32, magic: i32) {
-    println!("Health: {} || magic: {}", health, magic)
+fn stat_update(health: i32, max_health: i32, magic: i32, max_magic: i32) {
+    println!(
+        "Health: {}/{} || magic: {}/{}",
+        health, max_health, magic, max_magic
+    )
 }
